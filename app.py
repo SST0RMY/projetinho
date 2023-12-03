07d48b358948fee593f32ae19e53132e1a228261
SST0RMI <a85220@ualg.pt>
2023-12-03 18:36:53 +0000

    modifiquei o ficheiro app.py, adicionei a funcionalidade de alterar a password e o email, notificaçoes de ofertas marcação de test drive e a reserva de um veiculo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#Area do cliente
+
 class Cliente:
     def __init__(self, nome, email, senha):
         self.nome = nome
@@ -6,6 +6,7 @@ class Cliente:
         self.senha = senha
         self.carros_interesse = []
         self.carros_carrinho = []
+        self.notificacoes = []
 
     def adicionar_carro_interesse(self, carro):
         self.carros_interesse.append(carro)
@@ -14,6 +15,24 @@ class Cliente:
     def adicionar_ao_carrinho(self, carro):
         self.carros_carrinho.append(carro)
         print("Carro adicionado ao carrinho!")
+
+    def alterar_senha(self, nova_senha):
+        self.senha = nova_senha
+        print("Senha alterada com sucesso!")
+
+    def alterar_email(self, novo_email):
+        self.email = novo_email
+        print("Email alterado com sucesso!")
+
+    def receber_oferta(self, oferta):
+        self.notificacoes.append(oferta)
+        print("Oferta recebida!")
+
+    def marcar_test_drive(self, carro):
+        print(f"Test-drive marcado para o carro {carro.marca} {carro.modelo}!")
+
+    def reservar_veiculo(self, carro):
+        print(f"Veículo {carro.marca} {carro.modelo} reservado!")
 
 class Carro:
     def __init__(self, marca, modelo):
@@ -30,23 +49,41 @@ def main():
     cliente = Cliente("João", "joao@email.com", "senha123")
 
     # adicionar carros ao cliente de exemplo
+    print ("Interesses:")
     cliente.adicionar_carro_interesse(car1)
     cliente.adicionar_carro_interesse(car2)
     cliente.adicionar_carro_interesse(car3)
+    print("\n")
 
     # Adicionar a parte do carrinho
+    print ("Carrinho")
     cliente.adicionar_ao_carrinho(car1)
     cliente.adicionar_ao_carrinho(car3)
+    print("\n")
 
     # Mostrando os carros q interessam ao utilizador
+    print("\n")
     print(f"Carros de interesse do cliente {cliente.nome}:")
     for carro in cliente.carros_interesse:
         print(f"{carro.marca} {carro.modelo}")
 
     # carros no carrinho
-    print(f"Carros no carrinho do cliente {cliente.nome}:")
+    print("\n")
+    print(f"\nCarros no carrinho do cliente {cliente.nome}:")
     for carro in cliente.carros_carrinho:
         print(f"{carro.marca} {carro.modelo}")
+
+    # Exemplo de uso dos novos métodos
+    cliente.alterar_senha("novaSenha456")
+    cliente.alterar_email("novo@email.com")
+
+    oferta = "Oferta especial para você!"
+    cliente.receber_oferta(oferta)
+
+    car4 = Carro("BMW", "Série 3")
+    cliente.marcar_test_drive(car4)
+
+    cliente.reservar_veiculo(car2)
 
 if __name__ == "__main__":
     main()
